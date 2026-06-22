9d94e914b8107a351e3c4b0883b629aab0272466
jaykantyadavswe <jaykant.yadav.cse@gmail.com>
2026-06-22 16:30:35 +0530

    Initial Setup

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`);
+})
